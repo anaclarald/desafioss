ac2186845c204ef2498ee5886ec6f758f3dd690d
anaclarald <anaclaralimaduartx@gmail.com>
2024-11-18 17:43:27 -0300

    comit 4

--- Desafioss/src/app/app/desafio1/desafio1.component.ts
+++ Desafioss/src/app/app/desafio1/desafio1.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import { InputTextModule } from 'primeng/inputtext';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+
 @Component({
   selector: 'app-desafio1',
   standalone: true,
-  imports: [InputTextModule],
+  imports: [InputTextModule, ReactiveFormsModule],
   templateUrl: './desafio1.component.html',
   styleUrl: './desafio1.component.less'
 })
 export class Desafio1Component {
+  formGroup!: FormGroup;
 
+  ngOnInit() {
+    this.formGroup = new FormGroup({
+      text: new FormControl<string | null>(null),
+    });
+  }
 }
