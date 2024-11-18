f09792973254a3eb570b79d48aa13236cb76b131
anaclarald <anaclaralimaduartx@gmail.com>
2024-11-18 17:12:16 -0300

    comit 2

--- Desafioss/src/app/app.component.ts
+++ Desafioss/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Desafio1Component } from './app/desafio1/desafio1.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Desafio1Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.less'
 })
---
+++ Desafioss/src/app/app/desafio1/desafio1.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-desafio1',
+  standalone: true,
+  imports: [],
+  templateUrl: './desafio1.component.html',
+  styleUrl: './desafio1.component.less'
+})
+export class Desafio1Component {
+
+}
